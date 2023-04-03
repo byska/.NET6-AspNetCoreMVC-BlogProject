cbc3476376ceaac8bc20b48e00ef31a5b7e07b45
Beste Yasak <beyasak@hotmail.com>
2023-04-03 13:44:30 +0300

    //BusinessLayer

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetCategoryList()
+        {
+            //var categoryValues = cm.GetAllBL();
+            return View();
+        }
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+}
